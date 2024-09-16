4042904158a392a49b82a9fc857810ca56cf52c3
samina-shameem <samina.shameem.clt@gmail.com>
2024-09-16 14:14:29 +0200

    add files and folders and add codes

---
+++ src/stories/components/Login/Login.stories.jsx
@@ -0,0 +1,14 @@
+import { components } from "storybook/internal/components";
+import Login from "../Login";
+export default {
+    title:"FRK/components/Login",
+    component: Login,
+}
+export const Default = {
+    title: "FRK/components/Login",
+    component: Login,
+} 
+
+export const BreakingMyComponent ={
+
+}
---
+++ src/stories/components/Login/Password.stories.jsx
@@ -0,0 +1,13 @@
+import { components } from "storybook/internal/components";
+import Password from "../Password";
+export default {
+    title:"FRK/components/Password",
+    component: Password,
+}
+export const Default = {
+
+} 
+
+export const BreakingMyComponent ={
+
+}
